f37761231c0b3c352b63fcdf7b1dfc14f7540e66
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2016-01-31 22:01:54 -0500

    [develop] Create deploy script to make easier that job

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to Github...\033[0m"
+
+GITHUB_REPONAME="oskargicast/oskargicast.github.io"
+
+# Build the project.
+hugo
+
+# Moving the generated project.
+cp -R public /tmp
+cp CNAME /tmp/public
+
+# Going to "public" temporal project.
+cd /tmp/public
+git init
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="[master] Rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Adding remote.
+git remote add origin git@github.com:$GITHUB_REPONAME.git
+
+# Push source and build repos.
+git push origin master --force
+
+# Deleting temporal project.
+cd .. && rm -rf public
